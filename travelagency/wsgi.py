2a8757f719948c43fed4571a563926d79f8d8344
Godwin02 <godwinbmenachery2024a@mca.ajce.in>
2024-04-18 10:52:08 +0530

    second commit

--- travelagency/wsgi.py
+++ travelagency/wsgi.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 """
 WSGI config for travelagency project.
 
@@ -15,21 +15,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travelagency.settings')
 
 application = get_wsgi_application()
-=======
-"""
-WSGI config for travelagency project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travelagency.settings')
-
-application = get_wsgi_application()
->>>>>>> 4b41e64c6ce77e37f96011c147de728956266d81
